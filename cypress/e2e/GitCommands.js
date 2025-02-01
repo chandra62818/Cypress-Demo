b07ddcbd56b0ae20032ed2969933eafd0c83464c
Chandra Shekhar <2320468@cognizant.com>
2025-02-01 13:26:48 +0530

    FINAL COMMIT

---
+++ cypress/e2e/GitCommands.js
@@ -0,0 +1,28 @@
+// git init
+// git add . or filename
+// git commit -m 'comment'  
+// git branch -M main 	// creating new branch
+// git branch		// view current branch
+// git remote add origin repo_url
+// git push -u origin main  // provide uname & pwd
+//  (Git Actions)
+ 
+// (If facing SSH credential related  error)
+// ****************git config --global http.sslverify false
+
+// Under source control, if faced issue with credential, then in terminal
+ 
+// git config --global user.email "you@example.com"
+// git config --global user.name "YOUR USERNAME"
+
+// git init
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin //"ORIGIN URL"//
+// git push -u origin main
+
+// git remote add origin //"ORIGIN URL"//
+// git branch -M main
+// git push -u origin main
+
+//git remote remove origin
